7324b717fce4885cb66bbd7d2827b9bf74afc544
Stachelski <167793801+Stachelski@users.noreply.github.com>
2026-04-21 14:39:16 -0300

    Utilizando configurações com diferentes perfis

--- greeting-service/src/main/java/br/edu/atitus/greetingservice/GreetingServiceApplication.java
+++ greeting-service/src/main/java/br/edu/atitus/greetingservice/GreetingServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.greetingservice;
+package br.edu.atitus.greetingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
