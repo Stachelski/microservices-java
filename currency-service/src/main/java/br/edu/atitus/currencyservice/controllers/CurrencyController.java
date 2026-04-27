97dc8993883961382ef3211e27dd3ab0d94de20d
Stachelski <167793801+Stachelski@users.noreply.github.com>
2026-04-26 23:15:02 -0300

    currency-service

---
+++ currency-service/src/main/java/br/edu/atitus/currencyservice/controllers/CurrencyController.java
@@ -0,0 +1,50 @@
+package br.edu.atitus.currencyservice.controllers;
+
+import br.edu.atitus.currencyservice.dtos.CurrencyDTO;
+import br.edu.atitus.currencyservice.entities.CurrencyEntity;
+import br.edu.atitus.currencyservice.repositories.CurrencyRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("currency")
+public class CurrencyController {
+
+    @Value("${server.port}")
+    private String port;
+
+    private final CurrencyRepository repository;
+    private final Environment environment;
+
+    public CurrencyController(CurrencyRepository repository, Environment environment) {
+        this.repository = repository;
+        this.environment = environment;
+    }
+
+    @GetMapping("/convert")
+    public ResponseEntity<CurrencyDTO> getConvert(
+            @RequestParam String source,
+            @RequestParam String target) throws Exception {
+
+        source = source.toUpperCase();
+        target = target.toUpperCase();
+
+        CurrencyEntity currency = repository.findBySourceCurrencyAndTargetCurrency(source, target)
+                .orElseThrow(() -> new Exception ("Currency not found"));
+
+        String environment = "Currency-service running on port: " + port;
+
+        CurrencyDTO dto = new CurrencyDTO(currency.getSourceCurrency(),
+                currency.getTargetCurrency(),
+                currency.getConversionRate(),
+                environment);
+
+        return ResponseEntity.ok(dto);
+
+    }
+}
