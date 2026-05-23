bd5db7aa60bd974cc2921f7b5cc7c2d588e105e7
Stachelski <167793801+Stachelski@users.noreply.github.com>
2026-05-22 23:55:32 -0300

    Ajustes finais

--- currency-service/src/main/java/br/edu/atitus/currencyservice/clients/BCBClient.java
+++ currency-service/src/main/java/br/edu/atitus/currencyservice/clients/BCBClient.java
@@ -2,11 +2,18 @@ package br.edu.atitus.currencyservice.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "bcb-client", url= "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata")
+@FeignClient(
+        name = "bcb-client",
+        url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata"
+)
 public interface BCBClient {
-    //Consulta Data fixa 22/05/2026
-    @GetMapping("/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?%40moeda='{moeda}'&%40dataCotacao='05-22-2026'&%24format=json")
-    BCBResponse getBCBCurrency(@PathVariable String source);
+
+    @GetMapping(
+            "/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@dataCotacao='05-22-2026'&$format=json"
+    )
+    BCBResponse getBCBCurrency(
+            @RequestParam("@moeda") String moeda
+    );
 }
